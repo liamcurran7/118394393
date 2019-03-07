15ae1c670ddb6d3cacd47602fd5983c494bc56fa
118394393 <118394393@BISB73VL.bis.ad.ucc.ie>
2019-03-07 15:14:19 +0000

    Final Commit

--- ebus1.php
+++ ebus1.php
@@ -1,120 +1,137 @@
-<!--Creating a session variable and assigning it to txtTotal-->
+<!--Creating a session variable and assigning it to txtTotal -->
 <?php
 session_start();
 $totalValue = "";
 $_SESSION['txtTotal'] = $totalValue;
 ?>
 <!DOCTYPE html>
-<html lang ="en" xmins ="http.//www.w3.org/1999/xhtml">
+<html lang="en" xmins="https.//www.w3.org/1999/xhtml"
     <head>
-        <meta charset ="utf-8" />
-        <title> EBus1</title>
-        <link rel ="stylesheet" href ="style.css" type ="text/css" />
+        <style>
+            a{
+     
+  background-color: #f44336;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+        </style>
+          <meta charset="utf-8" />
+          <title> Calculator</title>
+          <link rel="stylesheet" href="style.css" type="text/css" />
     </head>
-    <body>
-        <div class ="form">
-            <form name ="intCalc" method ="post" action ="ebus2.php">
-                <h1> Shop Calculator </h1>
+    
+    <body style = background-color:yellow; background = https://images.pexels.com/photos/235994/pexels-photo-235994.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500>
+    <a href="http://localhost:8000/index.php">Return To Index</a>
+   
+        <div class="form">
+            <form name="intCalc" method="post" action="eBus2.php">
+                  
                 
                 <center>
                     <table cellspacing="10">
                         <tr>
-                            <td><b><h1 style ="background-color:white"> Select A Service </b> </td></h1>
+                          <td><b> <h1 style = font-size:40px> Select a Consulting Service:</b></td> 
                         </tr>
-                        <tr>
+                         <tr>
                             <td>Blockchain - $1000</td>
-                            <td> <input type ="radio" id ="Blockchain" name ="rdoGroup" value ="1000" /></td>
+                            <td><input type="radio" id="Blockchain" name="rdoGroup" value="1000" required="" /></td>
                         </tr>
                         <tr>
-                            <td> Autonomous Things (Robots)-$2000 </td>
-                            <td> <input type ="radio" id ="Autonomous" name ="rdoGroup" value ="2000" /></td>"
+                            <td>Autonomous Things (Robots) - $2000</td>
+                            <td><input type="radio" id="Autonomous" name="rdoGroup" value="2000" /></td>
                         </tr>
                         <tr>
-                            <td> Immersive Experience- $3000 </td>
-                            <td> <input type ="radio" id ="Immersive" name ="rdoGroup" value ="3000" /></td>
+                            <td>Immersive Experience - $3000</td>
+                            <td><input type="radio" id="Immersive" name="rdoGroup" value="3000" /></td>
+                        </tr>
+                    </table>
+                </center>
+                <br />
+                
+                <center> 
+                    <table cellspacing="10">
+                        <tr>
+                            <td><b></b></td>
+                              <td><b>Amount</b></td>
+                        </tr>
+                        <tr>
+                            <td>Sub Total</td>
+                            <td><input type="text" id="txtSubTot" name="txtSub" readonly /></td>
+                        </tr>
+                        <tr>
+                            <td>Discount @ 10%</td>
+                            <td><input type="text" id="txtDisc" name="txtDisc" readonly /></td>
+                        </tr>
+                        <tr>
+                            <td>+VAT @ 20%</td>
+                            <td><input type="text" id="txtVat" name="txtVat" readonly /></td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td><input type="text" id="txtTotal" name="txtTotal" value="" readonly /></td>
                         </tr>
                     </table>
                 </center>
                 <br />
                 <center>
-                    <table cellspacing ="10">
-                        <tr>
-                            <td><b></b></td>
-                            <td><b> Amount</b></td>
-                        </tr>
-                        <tr>
-                            <td> Sub Total</td>
-                            <td><input type ="text" id ="txtSubTotal" name ="txtSub" readonly /></td>
-                        </tr>
-                        <tr>
-                            <td> -Discount @10% </td>
-                            <td> <input type ="text" id ="txtDisc" name ="txtDisc" readonly /></td>
-                        </tr>
-                        <tr>
-                            <td> +VAT @20% </td>
-                            <td> <input type ="text" id ="txtVat" name ="txtVat" readonly /></td>
-                        </tr>
-                        <tr>
-                            <td>Total</td>
-                            <td><input type ="text" id ="txtTotal" name ="txtTotal" value ="" readonly /></td>
-                        </tr>
-                      
-                      
-           </table>
+                    <input type="button" name="btnCalc" id="btnCalc" onclick="calcSub()" value="Calculate Amount" />
+                    <input type="button" name="btnClear" id="btnClear" onclick="AmountClear()" value="Clear Choice" />
+                    <input type="submit" name="btnAdd" id="btnAdd" onclick="" value="Add to Shopping Cart" />
                 </center>
-                <br />
-                <center>
-                       <input type ="button" name ="btnCalc" id ="btnCalc" onclick ="calcSub()" value ="Calculate Amount"/>
-                       <input type ="button" name ="btnClear" id ="btnClear" onclick ="AmountClear()" value ="clear" />
-                       <input type ="submit" name ="btnProceed" id ="btnProceed" onclick ="" value ="Proceed"/>
-                </center>       
-                 
-                    
             </form>
         </div>
-        <script type ="text/javascript">
-            function calcsub() {
-                var subAmount = parseFloat(document.getElementById('txtSubTotal').value):
-                var Blockchain = parseFloat(document.getElementById('Blockchain').value):
-                var Autonomous Things = parseFloat(document.getElementById('Autonomous').value):
-                var Immersive Experience = parseFloat(document.getElementById('Immersive').value):
+        
+       <! Declaring variables and applying them to the prices->
+    <script type="text/javascript">
+            function calcSub() {
+                //Assigning variables to the values
+                var subAmount = parseFloat(document.getElementById('txtSubTot').value);
+                var Blockchain = parseFloat(document.getElementById('Blockchain').value);
+                var Autonomous = parseFloat(document.getElementById('Autonomous').value);
+                var Immersive = parseFloat(document.getElementById('Immersive').value);
                 
-                if (document.getElementById('Blockchain').checked) (
-                document.intCalc.txtSubTotal.value = Blockchain;
-                subAmount = Blockchain;
-                        calculation(subAmount);
-                        )
-                else if (document.getElementById('Autonomous').checked) (
-                        document.intCalc.txtSubTotal.value = Autonomous Things;
-                        subAmount = Autonomous;
-                                calculation(subAmount);
-                                        )
-                                else if (document.getElementById('Immersive').checked)(
-                                        document.intCalc.txtSubTotal.value = Immersive;
-                                        subAmount = Immersive;
-                                                calculation(subAmount);
-                                                        )
-                                                
-                                                function calculation(parmSTotal){
-                                                    var subTotal = parseFloat(parmSTotal);
-                                                    var discCalc = parseFloat(subTotal * .30)
-                                                    var vatCalc = parseFloat(subTotal * .40);
-                                                    var total = parseFloat(subTotal - discCalc + vatCalc);
-                                                    
-                                                    document.intCalc.txtDisc.value = discCalc;
-                                                    document.intCalc.txtVat.value = vatCalc;
-                                                    document.intCalc.txtTotal.value = total;
-                                                    
-                                                }
-                                                
-                                                function AmountClear(){
-                                                    document.getElementById("txtSubTotal").value = "";
-                                                    document.getElementById("txtDisc").value = "";
-                                                    document.getElementById("txtVat").value = "";
-                                                    document.getElementById("txtTotal").value = "";
-                                                }
-                        
-            </script>
+                //If radio buttons are clicked, values are assigned
+                if (document.getElementById('Blockchain').checked) {
+                    document.intCalc.txtSubTot.value = Blockchain;
+                    subAmount = Blockchain;
+                    calculation(subAmount);
+                }
+                else if (document.getElementById('Autonomous').checked) {
+                    document.intCalc.txtSubTot.value = Autonomous;
+                    subAmount = Autonomous;
+                    calculation(subAmount);
+            }
+            else if (document.getElementById('Immersive').checked) {
+                    document.intCalc.txtSubTot.value = Immersive;
+                    subAmount = Immersive;
+                    calculation(subAmount);
+                }
+            }
+            
+            //Function for calculating  the values
+            function calculation(parmSTotal) {
+                var subTotal = parseFloat(parmSTotal);
+                var discCalc = parseFloat(subTotal * .10);
+                var vatCalc = parseFloat(subTotal * .20);
+                var total = parseFloat(subTotal - discCalc + vatCalc);
+                
+                //Inserting them into the correct fields
+                document.intCalc.txtDisc.value = discCalc;
+                document.intCalc.txtVat.value = vatCalc;
+                document.intCalc.txtTotal.value = total;
+            }
+            
+            function AmountClear() {
+                document.getElementById('txtSubTot').value ="";
+                document.getElementById('txtDisc').value ="";
+                document.getElementById('txtVat').value ="";
+                document.getElementById('txtTotal').value ="";
+            }
+            
+        </script>
     </body>
 </html>
 
--- ebus2.php
+++ ebus2.php
@@ -1,53 +1,64 @@
 <?php
 session_start();
-$fullNameValue = "";
-$totalValue2 = "";
-
+$fullNameValue = "" ;
+$totalValue2="" ;
+/*
+ * Create a session variable for the mobile number
+ */
 $totalValue = $_POST['txtTotal'];
 $_SESSION['txtName'] = $fullNameValue;
-$_SESSION['TXTtOTAL'] = $totalValue2;
-
+$_SESSION['txtTotal']= $totalValue2 ;
+/**
+ * Allocate the mobile number session variable to a text
+ */
 ?>
-
-<!DOCTYPE   HTML>
+<!DOCTYPE html>
 <html>
-    <head>
-        <title> eBusiness 2 </title 
-    </head>
-    <body>
-        <div class ="form">
-            <form name ="Details" method ="post" action ="ebus3.php">
-                <center>
-                    <table cellspacing ="10">
-                        <tr>
-                            <td> <b> </b> </td>
-                            <td><b> Enter your details below</b> </td>
-                        </tr>
-                        <tr>
-                            <td>Name </td>
-                            <td> <input type ="text" id ="txtName" name ="txtName" value ="" /></td>
-                        </tr>
-                        <tr>
-                            <td> Phone Number</td>
-                            <td><input type ="text" id ="txtNum" name ="txtName" value ="" /></td>
-                            
-                        <tr><td>Password</td>
-                            <td><input type ="text" id ="txtPassword" name ="txtPassword" value ="" /></td>
-                        </tr>
-                        <tr>
-                            <td> <input type ="hidden" id ="txtTotal" name ="txtTotal" value ="<?php echo $totalValue;?>"/></td>
-                            
-                        </tr>
-                    </table>
-                </center>
-                <center>
-                    <input type ="submit" name ="btnContinue" id ="btnContinue" onclick ="" value ="Continue"/>
-                </center>
-        </div>
+<head>
+    <script>
+
+</script>
+    <title> eBusiness 2 </title>
+</head>
+<body background = https://images.pexels.com/photos/235994/pexels-photo-235994.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500>
+    <div class ="form">
+        <form name ="Details" method ="post" action= "eBUS3.php">
+            <center>
+               <! Creating the table that contains fields for personal details ->
+                <table style = cellspacing:40>                     <tr>
+                        <td><b></b></td>
+                        <td><b style = font-size:35px> Enter in your details below</b></td>
+                    </tr>
+                    
+                    <tr>
+                         <td> Name </td>
+                         <td><input type="text" id="txtName" name="txtName" value="" required="" /> </td>
+                    </tr>
+                    <br> <br>
+                     <tr>
+                         <td> Phone Number </td>
+                         <td><input type="text" id="txtNum" name="txtNum" value="" required /> </td>
+                     </tr> <br> <br>
+       
+                    <tr>
+                         <td> Password </td>
+                         <td><input type="text" id="txtPassword" name="txtPassword" value="" required /> </td>
+                    </tr>
+                    
+       
+                    <tr>
+                         <td><input type="Hidden" id="txtTotal" name="txtTotal" value="<?php echo $totalValue;?>" /> </td>
+                    </tr>
+       
+                </table>
+         </center>
+       
+       
+            <center>  
+        <input type="submit" name ="btnContinue" id="btnContinue" onclick="" value="Continue" />
+       
+            </center>
+    </div>
 </form>
-            
     </body>
-</html>
-
-
-
+    </html>
